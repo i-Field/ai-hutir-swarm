cb14b16aaeb378b4c1a1255dbc0d248035300972
Роман К. | iField <gabrielgarciamarkes@protonmail.com>
2026-05-18 13:07:57 -0700

    Initialize swarm420.py with environment variable loading

---
+++ swarm420.py
@@ -0,0 +1,10 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+HQ420_TOKEN = os.getenv("HQ420_TOKEN")
+QG420_TOKEN = os.getenv("QG420_TOKEN")
+QB420_TOKEN = os.getenv("QB420_TOKEN")
+
+print("AI HUTIR SWARM ONLINE 😁")
